3e93e21ffbe94ce47296a44a111277dc75da4cd6
Claus Niedermüller <claus.niedermueller@proton.me>
2024-01-21 20:45:02 +0100

    added load to status line

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -2,5 +2,6 @@
 
 datetime=$(date "+%a %Y-%m-%d %k:%M:%S")
 ram=$(free -h | grep Mem | awk '{printf "RAM: %s/%s\n", $3, $2}')
+load=$(cat /proc/loadavg | awk '{printf "load average: %s %s %s (%s)", $1, $2, $3, $4};')
 
-echo $ram $datetime
+echo "$load" "$ram" "$datetime"
