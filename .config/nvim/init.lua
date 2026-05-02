04a7b295c8981901d502383037d28890d5407b78
Claus Niedermüller <claus.niedermueller@proton.me>
2026-05-02 13:24:17 +0200

    rewrite neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,316 @@
-require('config')
+---@diagnostic disable: undefined-global
+
+-- general settings
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.confirm = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.completeopt = 'menu,menuone,fuzzy,noinsert'
+
+vim.opt.clipboard = 'unnamedplus'
+
+vim.g.mapleader = ' '
+vim.keymap.set('n', '<leader>o', vim.cmd.only)
+
+vim.keymap.set('v', 'J', ':m \'>+1<CR>gv=gv')
+vim.keymap.set('v', 'K', ':m \'<-2<CR>gv=gv')
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+--paste over selection without modifying the register
+vim.keymap.set('x', '<leader>p', '"_dP')
+
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+Y')
+
+--delete into void
+vim.keymap.set('n', '<leader>d', '"_d')
+vim.keymap.set('v', '<leader>d', '"_d')
+
+vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
+vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
+vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
+vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
+
+vim.keymap.set('n', '<leader>n', '<cmd>noh<CR>')
+
+vim.pack.add {
+  'https://github.com/folke/tokyonight.nvim',
+  'https://github.com/neovim/nvim-lspconfig',
+  'https://github.com/mason-org/mason.nvim',
+  'https://github.com/mason-org/mason-lspconfig.nvim',
+  'https://github.com/hrsh7th/cmp-nvim-lsp',
+  'https://github.com/hrsh7th/nvim-cmp',
+  'https://github.com/L3MON4D3/LuaSnip',
+  'https://github.com/saadparwaiz1/cmp_luasnip',
+  'https://github.com/rafamadriz/friendly-snippets',
+  'https://github.com/seblyng/roslyn.nvim',
+  'https://github.com/nvim-lua/plenary.nvim',
+  'https://github.com/nvim-telescope/telescope.nvim',
+  'https://github.com/nvim-telescope/telescope-fzf-native.nvim',
+  'https://github.com/nvim-tree/nvim-web-devicons',
+  'https://github.com/nvim-lualine/lualine.nvim',
+  'https://github.com/tpope/vim-fugitive',
+  'https://github.com/ThePrimeagen/harpoon',
+  'https://github.com/stevearc/oil.nvim',
+  'https://github.com/mfussenegger/nvim-dap',
+  'https://github.com/rcarriga/nvim-dap-ui',
+  'https://github.com/nvim-neotest/nvim-nio'
+}
+
+vim.cmd.packadd('nvim.undotree')
+vim.cmd.packadd('nvim.difftool')
+
+require("mason").setup({
+  registries = {
+    "github:mason-org/mason-registry",
+    "github:Crashdummyy/mason-registry",
+  },
+})
+require("mason-lspconfig").setup()
+require('lualine').setup()
+require("oil").setup({
+  view_options = {
+    show_hidden = true,
+  }
+})
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.cmd.colorscheme("tokyonight")
+
+--harpoon
+local mark = require('harpoon.mark')
+local ui = require('harpoon.ui')
+
+vim.keymap.set('n', '<leader>a', mark.add_file)
+vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
+
+--cmp
+local cmp = require 'cmp'
+require("luasnip.loaders.from_vscode").lazy_load()
+vim.keymap.set('i', '<C-K>', function()
+  require('luasnip').expand_or_jump()
+end)
+
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-u>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-c>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    -- { name = 'vsnip' }, -- For vsnip users.
+    { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- lsp
+vim.keymap.set('n', 'gl', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<leader>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<leader>=', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
+
+-- dap
+vim.keymap.set('n', '<leader>dt', ':DapToggleBreakpoint<CR>')
+vim.keymap.set('n', '<leader>dx', ':DapTerminate<CR>')
+vim.keymap.set('n', '<leader>dn', ':DapStepOver<CR>')
+vim.keymap.set('n', '<leader>di', ':DapStepInto<CR>')
+vim.keymap.set('n', '<leader>df', ':DapStepOver<CR>')
+
+vim.keymap.set('n', '<leader>dr', function() require('dap').repl.open() end)
+vim.keymap.set('n', '<leader>dl', function() require('dap').run_last() end)
+vim.keymap.set({ 'n', 'v' }, '<leader>dh', function()
+  require('dap.ui.widgets').hover()
+end)
+vim.keymap.set({ 'n', 'v' }, '<leader>dp', function()
+  require('dap.ui.widgets').preview()
+end)
+vim.keymap.set('n', '<leader>df', function()
+  local widgets = require('dap.ui.widgets')
+  widgets.centered_float(widgets.frames)
+end)
+vim.keymap.set('n', '<leader>ds', function()
+  local widgets = require('dap.ui.widgets')
+  widgets.centered_float(widgets.scopes)
+end)
+
+vim.keymap.set('n', '<leader>dc', function()
+  require('dap').continue()
+end)
+
+-- Dap Ui
+local dapui = require('dapui')
+local dap = require('dap')
+
+dapui.setup(
+  {
+    controls = {
+      element = 'repl',
+      enabled = true,
+      icons = {
+        disconnect = '',
+        pause = '',
+        play = '',
+        run_last = '',
+        step_back = '',
+        step_into = '',
+        step_out = '',
+        step_over = '',
+        terminate = ''
+      }
+    },
+    element_mappings = {},
+    expand_lines = true,
+    floating = {
+      border = 'single',
+      mappings = {
+        close = { 'q', '<Esc>' }
+      }
+    },
+    force_buffers = true,
+    icons = {
+      collapsed = '',
+      current_frame = '',
+      expanded = ''
+    },
+    layouts = { {
+      elements = { {
+        id = 'scopes',
+        size = 0.25
+      }, {
+        id = 'breakpoints',
+        size = 0.25
+      }, {
+        id = 'stacks',
+        size = 0.25
+      }, {
+        id = 'watches',
+        size = 0.25
+      } },
+      position = 'left',
+      size = 40
+    }, {
+      elements = { {
+        id = 'repl',
+        size = 0.5
+      }, {
+        id = 'console',
+        size = 0.5
+      } },
+      position = 'bottom',
+      size = 10
+    } },
+    mappings = {
+      edit = 'e',
+      expand = { '<CR>', '<2-LeftMouse>' },
+      open = 'o',
+      remove = 'd',
+      repl = 'r',
+      toggle = 't'
+    },
+    render = {
+      indent = 1,
+      max_value_lines = 100
+    }
+  }
+)
+
+dap.listeners.after.event_initialized['dapui_config'] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated['dapui_config'] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited['dapui_config'] = function()
+  dapui.close()
+end
+
+vim.keymap.set('n', '<leader>de', function() dapui.eval() end)
+
+
+-- dap adapters
+local mason_bin_dir = os.getenv('HOME') .. '/.local/share/nvim/mason/bin/'
+
+dap.adapters.codelldb = {
+  type = 'server',
+  port = '${port}',
+  executable = {
+    command = mason_bin_dir .. 'codelldb',
+    args = { '--port', '${port}' },
+  }
+}
+
+dap.adapters.coreclr = {
+  type = 'executable',
+  command = mason_bin_dir .. 'netcoredbg',
+  args = { '--interpreter=vscode' }
+}
+
+-- telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
